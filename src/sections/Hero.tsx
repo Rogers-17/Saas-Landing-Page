cdf5088534f626b48118eb8f304dd4835f122fbd
Rogers <mohammedrogers579@gmail.com>
2025-10-04 10:49:00 -0700

    Added Medias for Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,14 +1,36 @@
 import Button from "@/components/Button";
+import designExample1 from '@/assets/images/design-example-1.png'
+import designExample2 from '@/assets/images/design-example-2.png'
+import Image from "next/image";
+import Pointer from "@/components/Pointer";
 
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-32 top-16 hidden lg:block ">
+                    <Image 
+                        src={designExample1}
+                        alt="Example image"
+                    />
+                </div>
+                <div className="absolute -right-64 -top-16 hidden lg:block ">
+                    <Image 
+                        src={designExample2}
+                        alt="Example image"
+                    />
+                </div>
+                <div className="absolute left-56 top-96 hidden lg:block ">
+                    <Pointer name="Andrea"/>
+                </div>
+                <div className="absolute right-80 -top-4 hidden lg:block ">
+                    <Pointer name="Brian" color="red"/>
+                </div>
                 <div className="flex justify-center ">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                     ✨$7.5M seed round raised</div>
                 </div>
-                <h1 className="text-6xl md:text-7xl font-medium text-center mt-6">Impactful design, created effortlessly</h1>
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">Impactful design, created effortlessly</h1>
                 <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">Design tools shouldn&apos;t slow you down. Layers combines powerful features with an intutiative interface
                     that keeps you in your creative flow.
                 </p>
